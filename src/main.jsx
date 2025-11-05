c5342bae5e6f7417d2451efcad27f951450939f7
Kavin-0307 <sharmakavin03@gmail.com>
2025-11-05 21:42:35 +0530

    Add ProtectedRoute guarding

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,29 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import JournalListPage from "./pages/JournalListPage";
 import "./theme.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="/" element={<AppLayout />}>
-      <Route index element={<JournalListPage />} />
-      <Route path="journals" element={<JournalListPage />} />
-      <Route path="journals/new" element={<EntryEditorPage />} />
-      <Route path="journals/:id/edit" element={<EntryEditorPage />} />
-    </Route>
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-
+      {/* ✅ Protect everything inside AppLayout */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<JournalListPage />} />
+        <Route path="journals" element={<JournalListPage />} />
+        <Route path="journals/new" element={<EntryEditorPage />} />
+        <Route path="journals/:id/edit" element={<EntryEditorPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
